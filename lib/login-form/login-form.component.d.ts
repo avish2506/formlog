ba8807c6008325a874543c8db0aeb0960da53015
Abish kumar <abhishkumar.k@particleblack.com>
2023-07-13 11:53:21 +0530

    v10
    v10

--- lib/login-form/login-form.component.d.ts
+++ lib/login-form/login-form.component.d.ts
@@ -1,6 +1,21 @@
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import * as i0 from "@angular/core";
 export declare class LoginFormComponent {
-    getValues(val: any): void;
+    private fb;
+    private _router;
+    loginForm: FormGroup;
+    show: boolean;
+    disabledLoginPage: boolean;
+    submitStatus: string;
+    otpForm: any;
+    input: any;
+    constructor(fb: FormBuilder, _router: Router);
+    initializeForm(): void;
+    initializeOtpForm(): void;
+    routeToChangePassword(): void;
+    toggleShow(): void;
+    login(val: any): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<LoginFormComponent, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<LoginFormComponent, "lib-login-form", never, {}, {}, never, never, false, never>;
 }
